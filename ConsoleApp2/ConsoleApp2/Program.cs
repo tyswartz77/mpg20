b55064da2e175c29b5ebfbce60ae10ec36f161b7
Ty Swartzentruber <swartztj1@grace.edu>
2020-09-02 15:19:05 -0400

    added mpg calc

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Miles per Gallon");
+            double mpg = 500 / 27;
+            Console.WriteLine("you got " + mpg + " miles per gallon");
+        }
+    }
+}
